603e7c166d152843d170193ef621cc099c2bd003
Gautam-code-19 <gautamsinghji2002@gmail.com>
2024-04-18 12:52:10 +0530

    form validation completed

--- Invoice/Invoice-form.js
+++ Invoice/Invoice-form.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
-  const Addmore = ` <tr>
-                                <td><input type="text" name="Item" placeholder="Item Name"></td>
-                                <td><input type="text" name="description" placeholder="Description"></td>
-                                <td><input type="number" name="Rate" placeholder="Price per unit"></td>
-                                <td><input type="number" name="Units" value="1" placeholder="No. of units"></td>
-                            </tr>`;
-  const tbody = $("#tbody");
+  let Idis = 2;
+
   $("#addItem").on("click", function () {
-    tbody.append(Addmore);
+    let Addmore = `<tr id="${Idis}" >
+                                    <td><input type="text" name="Item" placeholder="Item Name"></td>
+                                    <td><input type="text" name="description" placeholder="Description"></td>
+                                    <td><input type="number" name="Rate" placeholder="Price per unit"></td>
+                                    <td class="d-flex"><input type="number" name="Units" value="1"
+                                            placeholder="No. of units">
+                                        <button type="button" class="delete-left" title='Delete' onclick="deleteOne(${Idis})">
+                                            <i class="fa-solid fa-delete-left delete-left-icon"></i>
+                                        </button>
+                                    </td>
+                                </tr>`;
+    $("#tbody").append(Addmore);
+    Idis++;
   });
 });
+
+function deleteOne(withid) {
+  if (window.confirm("Confirm  if you want to Delete !")) {
+    $("#" + withid).remove();
+  }
+}
---
+++ Invoice/formValidation.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+  jQuery("#invoice-form").validate({
+    rules: {
+      comName: {
+        required: true,
+        minlength: 3,
+      },
+      address: {
+        required: true,
+      },
+      city: {
+        required: true,
+      },
+      mypin: {
+        required: true,
+        minlength: 6,
+      },
+      country: {
+        required: true,
+      },
+      phone: {
+        required: true,
+        minlength: 10,
+        maxlength: 10,
+      },
+
+      // client side form validation
+      clientName: {
+        required: true,
+        // lettersonly: true,
+        minlength: 3,
+      },
+      clientAdd: {
+        required: true,
+      },
+      clientCity: {
+        required: true,
+      },
+      clientPin: {
+        required: true,
+        minlength: 6,
+        maxlength: 6,
+      },
+      clientCountry: {
+        required: true,
+      },
+      clientPhone: {
+        required: true,
+        maxlength: 10,
+        minlength: 10,
+      },
+      // invoice Details..
+      issueDate: {
+        required: true,
+      },
+      invoiceNumber: {
+        required: true,
+      },
+      referanceNumber: {
+        required: true,
+      },
+      dueDate: {
+        required: true,
+      },
+
+      // Items details...
+      Item: {
+        required: true,
+      },
+      description: {
+        required: true,
+      },
+      Rate: {
+        required: true,
+      },
+      Units: {
+        required: true,
+      },
+    },
+    messages: {
+      comName: {
+        required: "Company name required.*",
+        minlength: "Atleast 3 letters required.",
+      },
+      address: {
+        required: "This field is required.*",
+      },
+      city: {
+        required: "City is Required.*",
+      },
+      mypin: {
+        required: "Pin-Code missed.*",
+      },
+      country: {
+        required: "Required.*",
+      },
+
+      // client side form validation
+      clientName: {
+        required: "Client Name Required.*",
+        // lettersonly: "Only Characters allowed.*",
+        minlength: "At least 3 letters required",
+      },
+      clientAdd: {
+        required: "This field is required.*",
+      },
+      clientCity: {
+        required: "City is Required.*",
+      },
+      clientPin: {
+        required: "Pin-Code missed.*",
+      },
+      clientCountry: {
+        required: "Required.*",
+      },
+    },
+  });
+});
